7665bf95d6c167bf661542ab00335f351371ae41
Buywatermelon <1505127742@qq.com>
2020-02-12 12:59:09 +0800

    去除一些注释

--- src/nio/Client.java
+++ src/nio/Client.java
@@ -27,41 +27,44 @@ public class Client {
             "localhost", 8888);
 
     public static void main(String[] args) throws IOException {
-        // 打开socket通道
+
         SocketChannel socketChannel = SocketChannel.open();
-        // 设置为非阻塞方式
+
         socketChannel.configureBlocking(false);
-        // 打开选择器
+
         Selector selector = Selector.open();
-        // 注册连接服务端socket动作
+
         socketChannel.register(selector, SelectionKey.OP_CONNECT);
-        // 连接
+
         socketChannel.connect(SERVER_ADDRESS);
-        // 分配缓冲区大小内存
+
 
         Set<SelectionKey> selectionKeys;
         Iterator<SelectionKey> iterator;
         SelectionKey selectionKey;
         SocketChannel client;
+
         String receiveText;
         String sendText;
-        // 判断是否有接收数据
+
         int count;
 
         while (true) {
-            //选择一组键，其相应的通道已为 I/O 操作准备就绪。
-            //此方法执行处于阻塞模式的选择操作。
+
             selector.select();
-            //返回此选择器的已选择键集。
+
             selectionKeys = selector.selectedKeys();
             iterator = selectionKeys.iterator();
+
             while (iterator.hasNext()) {
+
                 selectionKey = iterator.next();
+
                 if (selectionKey.isConnectable()) {
                     System.out.println("client connect");
                     client = (SocketChannel) selectionKey.channel();
-                    // 判断此通道上是否正在进行连接操作。
-                    // 完成套接字通道的连接过程。
+
+                    // 此通道是否正在进行连接操作。
                     if (client.isConnectionPending()) {
                         client.finishConnect();
                         System.out.println("完成连接!");
@@ -70,13 +73,17 @@ public class Client {
                         sendbuffer.flip();
                         client.write(sendbuffer);
                     }
+
                     client.register(selector, SelectionKey.OP_READ);
+
                 } else if (selectionKey.isReadable()) {
+
                     client = (SocketChannel) selectionKey.channel();
-                    //将缓冲区清空以备下次读取
+
                     receivebuffer.clear();
-                    //读取服务器发送来的数据到缓冲区中
+
                     count=client.read(receivebuffer);
+
                     if(count>0){
                         receiveText = new String( receivebuffer.array(),0,count);
                         System.out.println(receiveText);
@@ -84,13 +91,19 @@ public class Client {
                     }
 
                 } else if (selectionKey.isWritable()) {
+
                     sendbuffer.clear();
+
                     client = (SocketChannel) selectionKey.channel();
+
                     sendText = clientOperation();
+
                     sendbuffer.put(sendText.getBytes());
-                    //将缓冲区各标志复位,因为向里面put了数据标志被改变要想从中读取数据发向服务器,就要复位
+
                     sendbuffer.flip();
+
                     client.write(sendbuffer);
+
                     client.register(selector, SelectionKey.OP_READ);
                 } 
             }
@@ -106,6 +119,7 @@ public class Client {
         System.out.println("请输入您的操作：A；领养小狗  B：领养小猫  C：领养鹦鹉  D：领养小鸡  E:查看宠物受欢迎程度");
 
         String chars = "ABCDE";
+
         String randomOperation = String.valueOf(chars.charAt((int) (Math.random() * 5)));
         System.out.println(randomOperation);
 
--- src/nio/Server.java
+++ src/nio/Server.java
@@ -42,20 +42,19 @@ public class Server {
     private String sendText = null;
 
     public Server(int port) throws IOException {
-        // 打开服务器套接字通道
-        // 底层: 在linux上面开启socket服务，启动一个线程。绑定ip地址和端口号
+
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        // 服务器配置为非阻塞
+
         serverSocketChannel.configureBlocking(false);
-        // 检索与此通道关联的服务器套接字
+
         ServerSocket serverSocket = serverSocketChannel.socket();
-        // 进行服务的绑定
+
         serverSocket.bind(new InetSocketAddress(port));
-        // 通过open()方法找到Selector
-        // 底层： 开启epoll，为当前socket服务创建epoll服务，epoll_create
+
         selector = Selector.open();
-        // 注册到selector，等待连接
+
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
         System.out.println("Server Start----8888:");
     }
 
@@ -66,14 +65,14 @@ public class Server {
      */
     private void listen() throws IOException {
         while (true) {
-            // 选择一组键，并且相应的通道已经打开
+
             selector.select();
-            // 返回此选择器的已选择键集。
+
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
             while (iterator.hasNext()) {
                 SelectionKey selectionKey = iterator.next();
-                // 获得当前epoll的rdlist复制到用户态，遍历，同事删除当前rdlist中的事件
                 iterator.remove();
                 handleKey(selectionKey);
             }
@@ -87,44 +86,48 @@ public class Server {
      * @throws IOException
      */
     private void handleKey(SelectionKey selectionKey) throws IOException {
-        // 接受请求
+
         ServerSocketChannel server;
         SocketChannel client;
-        int count=0;
-        // 测试此键的通道是否已准备好接受新的套接字连接。
+
+        int count;
+
         if (selectionKey.isAcceptable()) {
-            // 返回为之创建此键的通道。
+
             server = (ServerSocketChannel) selectionKey.channel();
-            // 接受到此通道套接字的连接。
-            // 此方法返回的套接字通道（如果有）将处于阻塞模式。
+
             client = server.accept();
-            // 配置为非阻塞
+
             client.configureBlocking(false);
-            // 注册到selector，等待连接
+
             client.register(selector, SelectionKey.OP_READ);
+
         } else if (selectionKey.isReadable()) {
-            // 返回为之创建此键的通道。
+
             client = (SocketChannel) selectionKey.channel();
-            //将缓冲区清空以备下次读取
+
             receiveBuffer.clear();
-            //读取服务器发送来的数据到缓冲区中
+
             count = client.read(receiveBuffer);
+
             if (count > 0) {
                 receiveText = new String( receiveBuffer.array(),0,count);
                 sendText = switchReceiveText(receiveText);
                 client.register(selector, SelectionKey.OP_WRITE);
             }
+
         } else if (selectionKey.isWritable()) {
-            //将缓冲区清空以备下次写入
+
             sendBuffer.clear();
-            // 返回为之创建此键的通道。
+
             client = (SocketChannel) selectionKey.channel();
-            //向缓冲区中输入数据
+
             sendBuffer.put(sendText.getBytes());
-            //将缓冲区各标志复位,因为向里面put了数据标志被改变要想从中读取数据发向服务器,就要复位
+
             sendBuffer.flip();
-            //输出到通道
+
             client.write(sendBuffer);
+
             client.register(selector, SelectionKey.OP_READ);
         }
     }
